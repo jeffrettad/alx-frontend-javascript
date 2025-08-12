75af91fd7ffb6bb46776863512aa20ec771f092e
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-08-12 03:57:02 -1200

    fix main error

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -41,13 +41,13 @@ export function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-export function isDirector(employee: Director | Teacher): employee is Director {
-  return employee instanceof Director;
-}
 
 export function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
   }
   return employee.workTeacherTasks();
+}
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
 }
